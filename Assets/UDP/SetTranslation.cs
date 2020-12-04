27eae4ca5f033db3184b30cf063cddac34cc3289
Elias Fandi <elias@fandi.me>
2020-12-04 13:40:07 +0100

    Comments

--- Assets/UDP/SetTranslation.cs
+++ Assets/UDP/SetTranslation.cs
@@ -5,6 +5,7 @@ public class SetTranslation : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		print("updated car pos" + UDPData.xFloat);
 		transform.position = new Vector3(UDPData.xFloat, UDPData.yFloat, UDPData.zFloat);
 
 	}
