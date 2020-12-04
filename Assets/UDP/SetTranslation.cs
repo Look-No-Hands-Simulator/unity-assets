40ec30703b494af30cf4218a6fcc0fc56c49e3d9
Elias Fandi <elias@fandi.me>
2020-12-04 18:29:44 +0100

    Set rotation (Inverted from Gazebo)

--- Assets/UDP/SetTranslation.cs
+++ Assets/UDP/SetTranslation.cs
@@ -8,5 +8,9 @@ public class SetTranslation : MonoBehaviour {
 		print("updated car pos" + UDPData.xFloat);
 		transform.position = new Vector3(UDPData.xFloat, UDPData.yFloat, UDPData.zFloat);
 
+		transform.rotation = new Quaternion(UDPData.pFloat, UDPData.qFloat, UDPData.rFloat, UDPData.wFloat);
+		transform.rotation = Quaternion.Inverse(transform.rotation);
+
+
 	}
 }
